bed66289a773efd86e17b2b3af86ea90fc20ebe1
bbbhs <111858611+bbbhs@users.noreply.github.com>
2025-04-10 12:41:59 +0900

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(148, 72);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(121, 52);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "확인";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 325);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(793, 127);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 464);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Form111111";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.RichTextBox txtLog;
+    }
+}
+
+
+
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static RichTextBox m_hLog;
+        public static void logWrite(string msg,bool bNewline = true)
+        {
+            if (m_hLog is null) return;
+            m_hLog.AppendText($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} {msg} {(bNewline ? System.Environment.NewLine : " ")}";
+        }
+
+        public void logLink()
+        {
+            m_hLog = this.txtLog;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            logLink();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("확인 버튼이 눌렸습니다");
+            Button btn = (Button)sender;
+            //btn.Enabled = false;
+            logWrite($"[INFO] {btn.Name}::Clicked");            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
